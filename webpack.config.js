709330161f6531e437e388db358fd715d00f6fc8
Ansh Gujral <anshgujral1997@gmail.com>
2020-05-31 15:35:10 +0530

    repository setup
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = ({ env }) => {
+    const analyzer = env === "analyze" ?
+        [
+            new BundleAnalyzerPlugin({
+                analyzerMode: "static",
+                reportFilename: "./report.html",
+                openAnalyzer: true,
+            })] : []
+
+    return {
+        mode: "production",
+        entry: "./src/index.tsx",
+        resolve: {
+            extensions: [".js", ".ts", ".tsx"]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"],
+                },
+                {
+                    test: /\.(tsx|js|ts)$/,
+                    loader: "babel-loader",
+                    exclude: /node-modules/,
+                    options: {
+                        presets: [
+                            "@babel/react",
+                            "@babel/typescript",
+                            ["@babel/env", { targets: { browsers: ["last 2 versions"] } }]
+                        ],
+                        plugins: [
+                            "@babel/plugin-proposal-class-properties",
+                            "@babel/plugin-proposal-export-default-from",
+                            "@babel/plugin-syntax-dynamic-import"
+                        ]
+                    }
+                }
+            ],
+        },
+        plugins: [
+            new HtmlWebPackPlugin({
+                template: './src/index.html',
+                filename: 'index.html',
+            })
+        ]
+    }
+};
