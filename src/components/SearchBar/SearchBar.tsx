9407a35925defc7cda7c651e999e16e171588c33
Ansh Gujral <anshgujral1997@gmail.com>
2020-05-31 20:45:41 +0530

    create  searchbar component
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,28 @@
+import React, { memo, FC } from "react";
+import "./searchBar.css";
+
+interface SearchBarProps {
+  value: string;
+  onChangeInput: (value: string) => void;
+}
+
+const SearchBar: FC<SearchBarProps> = ({ value, onChangeInput }) => {
+  return (
+    <div className="search-wrap">
+      <div className="search">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search users..."
+          value={value}
+          onChange={(e) => onChangeInput(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          &#128269;
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default memo(SearchBar);
